7614193898b70e593d8fdc8b26de2cdfd564b43f
Noman AWan <awanzada408@gmail.com>
2024-12-28 01:47:44 +0500

    update 2.3

--- app/Mail/UserApproved.php
+++ app/Mail/UserApproved.php
@@ -2,36 +2,23 @@
 
 namespace App\Mail;
 
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
-use Illuminate\Queue\SerializesModels;
+use Illuminate\Mail\Mailables\Attachment;
 
 class UserApproved extends Mailable
 {
-    use Queueable, SerializesModels;
-
-    public $user;
-    /**
-     * Create a new message instance.
-     */
-    public function __construct($user)
-    {
-        $this->user = $user;
-    }
+    public function __construct(public $user) {}
 
     /**
      * Get the message envelope.
      */
     public function envelope(): Envelope
     {
-        return $this->subject('Your Account Has Been Approved')
-                    ->view('emails.user-approved')
-                    ->with([
-                        'name' => $this->user->name,
-                    ]);
+        return new Envelope(
+            subject: 'User Approved',
+        );
     }
 
     /**
@@ -40,14 +27,15 @@ class UserApproved extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'view.name',
+            view: 'emails.user-approved',
+            data: ['user' => $this->user], // Pass data directly to the view
         );
     }
 
     /**
      * Get the attachments for the message.
      *
-     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     * @return array<int, Attachment>
      */
     public function attachments(): array
     {
