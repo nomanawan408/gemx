f2dd90907388c11e4bf68da249076878de6969f0
Noman AWan <awanzada408@gmail.com>
2025-02-01 17:25:55 +0500

    decending order and time zone setting

--- app/Http/Controllers/VisitorsController.php
+++ app/Http/Controllers/VisitorsController.php
@@ -12,7 +12,7 @@ class VisitorsController extends Controller
      */
     public function index()
     {
-        $users = User::role('visitor')->orderByDesc('id')->latest()->get();
+        $users = User::role('visitor')->latest()->get();
                 return view('visitors.index', compact('users'));
     }
 
