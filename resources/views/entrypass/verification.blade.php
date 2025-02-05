3b0ff83ac25c1947186455e0a314667ecde28ede
Noman AWan <awanzada408@gmail.com>
2025-02-05 17:53:55 +0500

    qrcode fixed

--- resources/views/entrypass/verification.blade.php
+++ resources/views/entrypass/verification.blade.php
@@ -9,16 +9,35 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 
-<body class="bg-success text-white">
+<body style="background: #046120 " class=" text-white">
     <div class="container text-center py-5">
         <h1>Verification</h1>
         <div class="card bg-transparent border-0">
             <div class="card-body">
-                <p class="card-text">Role: {{ $role }}</p>
-                <p class="card-text">User ID: {{ $user->id }}</p>
-                <p class="card-text">Name: {{ $user->name }}</p>
-                <p class="card-text">CNIC/Passport: {{ $user->cnic_passport_no }}</p>
-                <p class="card-text">Company Name: {{ $company_name }}</p>
+                <table class="table table-borderless text-white">
+                    <tbody>
+                        <tr>
+                            <th scope="row">Role</th>
+                            <td>{{ $role }}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">User ID</th>
+                            <td>{{ $user->id }}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Name</th>
+                            <td>{{ $user->name }}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">CNIC/Passport</th>
+                            <td>{{ $user->cnic_passport_no }}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Company Name</th>
+                            <td>{{ $company_name }}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
