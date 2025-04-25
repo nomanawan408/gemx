8b8380ec198c35caf879c6d35d3a46a955baf1e5
Noman AWan <awanzada408@gmail.com>
2025-04-25 18:35:15 +0500

    new update 1

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -149,7 +149,7 @@
                                                     {{-- ---------------------- For Exhibitors --------------- --}}
                                                     @if ($user->hasRole('exhibitor'))
                                                         <li><strong>Chamber/Association Membership:</strong>
-                                                            @json($user->business->chamber_association_membership)</li>
+                                                            {{ $user->business->chambesssr_association_membership }}</li>
                                                         <li><strong>Nature of Business:</strong>
                                                             {{ $user->business->nature_of_business }}</li>
                                                         <li><strong>Type of Business:</strong>
